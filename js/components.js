bd96512c4af18aece4683955c4c6031b4737c4a3
liamsufc <w22013172@northumbria.ac.uk>
2025-01-31 22:28:33 +0000

    Initial Website Structure and Content
    Initial commit containing index, portfolio and contact pages, as well as other website files.

---
+++ js/components.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("header").innerHTML = `
+        <header>
+            <nav>
+                <div class="menu-toggle" onclick="toggleMenu()">☰</div>
+                <ul id="nav-links">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    `;
+
+    document.getElementById("footer").innerHTML = `
+        <footer>
+            <p>Liam Thomas - ${new Date().getFullYear()}</p>
+            <a href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by Delwar018 - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by Freepik - Flaticon</a>
+        </footer>
+    `;
+});
---
+++ js/portfolio.js
@@ -0,0 +1,39 @@
+const projects = [
+    {
+        title: "Evolution Designers LLP",
+        image: "images/evolution-designers-logo.webp",
+        url: "https://evolution-designers.com",
+        desc: "A partnership I founded specialising in website creation.",
+    },
+    {
+        title: "United Homes Website",
+        image: "images/utdhomes-project.png",
+        url: "https://www.utdhomes.co.uk",
+        desc: "A website I made for an estate agents in Oughtibridge.",
+    },
+    {
+        title: "City Road Community Childcare Website",
+        image: "images/crcc-project.png",
+        url: "https://cityroadchildcare.co.uk",
+        desc: "A website I made for a childcare establishment in Sheffield.",
+    },
+    {
+        title: "Queen of Diamonds Casino Hire Project",
+        image: "images/queenofdiamonds-project.png",
+        url: "http://www.queenofdiamondscasino.co.uk",
+        desc: "A website I made for a casino hire service in Doncaster",
+    }
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+    let portfolioContainer = document.getElementById("portfolio-container");
+    projects.forEach((project) => {
+        portfolioContainer.innerHTML += `
+            <a href="${project.url}" class="project" target="_blank">
+                <img src="${project.image}" alt="${project.title}">
+                <h3>${project.title}</h3>
+                <p class="project-desc">${project.desc}</p>
+            </a>
+        `;
+    });
+});
